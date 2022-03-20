2f203145aefd2b7e4cd08d3270130017b36d884c
Camila Andrade <camila.andraderk@gmail.com>
2022-03-20 20:31:09 -0300

    Ajustes na barra de pesquisa

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -5,11 +5,8 @@ import { fecharBarraPesquisa } from './barra-pesquisa.js';
 $(function(){
 
     /* Abrindo e fechando barra de pesquisa */
-    const iconeAbrirPesquisa = $('.lupa-individual');
-
     const elementoAbrirBarraPesquisa = $('.lupa-individual__lupa');
     const elementoFecharBarraPesquisa = $('.lupa-individual__fechar');
-    const barraPesquisa = $('barra-pesquisa');
 
     elementoAbrirBarraPesquisa.click(function(){
         abrirBarraPesquisa($(this), elementoFecharBarraPesquisa);

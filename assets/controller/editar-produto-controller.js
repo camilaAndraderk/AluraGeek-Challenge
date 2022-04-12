6943a9f2d7528d2b107efd7d746a013bd5a74199
Camila Andrade <camila.andraderk@gmail.com>
2022-04-12 13:48:23 -0300

    buscando dados para edição

--- assets/controller/editar-produto-controller.js
+++ assets/controller/editar-produto-controller.js
@@ -1,4 +1,19 @@
-(()=>{
+import { produtoService } from "../../service/produto-service.js";
+
+(async () => {
+    const elementoImg = $('[data-img-produto]');
+    const elementoNome = $('[data-nome-produto]');
+    const elementoCategoria = $('[data-categoria-produto]');
+    const elementoValor = $('[data-valor-produto]');
+    const elementoDescricao = $('[data-descricao-produto]');
+
     const url = new URL(window.location);
-    console.log(url);
+    const idPRodutoEditar = url.searchParams.get('id');
+    const produto = await produtoService.buscarProdutosId(idPRodutoEditar);
+
+    elementoImg.attr('src', produto.img);
+    elementoNome.val(produto.nome);
+    elementoCategoria.val(produto.categoria);
+    elementoValor.val(produto.valor);
+    elementoDescricao.val(produto.descricao);
 })();
